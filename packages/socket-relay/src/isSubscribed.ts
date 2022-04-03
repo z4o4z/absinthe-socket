651d6a529968b99d60d77a219a993c26d576ef2e
Evgeny <overhawlin@gmail.com>
2022-04-03 17:29:46 +0200

    fix: Relay build

--- packages/socket-relay/src/isSubscribed.ts
+++ packages/socket-relay/src/isSubscribed.ts
@@ -1,13 +1,13 @@
-import type { Disposable } from 'react-relay';
+import type { Observable } from 'relay-runtime';
 
 import subscriptions from './subscriptions';
 
 /**
  * Returns a promise that resolves to `true` in case subscription of given
- * disposable has started or to `false` otherwise
+ * observable has started or to `false` otherwise
  */
-const isSubscribed = (disposable: Disposable): Promise<boolean> => {
-  const maybeSubscription = subscriptions.get(disposable);
+const isSubscribed = (observable: Observable<any>): Promise<boolean> => {
+  const maybeSubscription = subscriptions.get(observable);
 
   return maybeSubscription ? maybeSubscription.then(() => true).catch(() => false) : Promise.resolve(false);
 };
