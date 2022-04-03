b2cc6e517cfbb169595f43e5f2a0153f72a8a294
Evgeny <overhawlin@gmail.com>
2022-04-03 17:37:59 +0200

    fix: Relay export

--- packages/socket-apollo-link/src/createAbsintheSocketLink.ts
+++ packages/socket-apollo-link/src/createAbsintheSocketLink.ts
@@ -1,54 +1,61 @@
-// @flow
+import { EmptyObject } from '@absinthe/graphql-utils';
+import type { AbsintheSocket, GqlRequest, Notifier, Observer } from '@absinthe/socket';
+import { send, toObservable, unobserveOrCancel } from '@absinthe/socket';
+import { ApolloLink } from 'apollo-link';
+import { print } from 'graphql';
+import type { DocumentNode } from 'graphql/language/ast';
 
-import {ApolloLink} from "apollo-link";
-import {send, toObservable, unobserveOrCancel} from "@absinthe/socket";
-import {compose} from "flow-static-land/lib/Fun";
-import {print} from "graphql";
+type ApolloOperation<Variables> = {
+  query: DocumentNode;
+  variables: Variables;
+};
 
-import type {AbsintheSocket, GqlRequest, Observer} from "@absinthe/socket";
-import type {DocumentNode} from "graphql/language/ast";
-
-type ApolloOperation<Variables> = {|
-  query: DocumentNode,
-  variables: Variables
-|};
-
-const unobserveOrCancelIfNeeded = (absintheSocket, notifier, observer) => {
+const unobserveOrCancelIfNeeded = <Result, Variables extends void | EmptyObject>(
+  absintheSocket: AbsintheSocket,
+  notifier?: Notifier<Result, Variables>,
+  observer?: Observer<Result, Variables>,
+): void => {
   if (notifier && observer) {
     unobserveOrCancel(absintheSocket, notifier, observer);
   }
 };
 
-const notifierToObservable = (absintheSocket, onError, onStart) => notifier =>
-  toObservable(absintheSocket, notifier, {
-    onError,
-    onStart,
-    unsubscribe: unobserveOrCancelIfNeeded
-  });
+const notifierToObservable =
+  <Result, Variables extends void | Object>(
+    absintheSocket: AbsintheSocket,
+    onError: Observer<Result, Variables>['onError'],
+    onStart: Observer<Result, Variables>['onStart'],
+  ) =>
+  (notifier: Notifier<Result, Variables>) =>
+    toObservable(absintheSocket, notifier, {
+      onError,
+      onStart,
+      unsubscribe: unobserveOrCancelIfNeeded,
+    });
 
-const getRequest = <Variables: Object>({
+const getRequest = <Variables extends Object>({
   query,
-  variables
+  variables,
 }: ApolloOperation<Variables>): GqlRequest<Variables> => ({
   operation: print(query),
-  variables
+  variables,
 });
 
 /**
  * Creates a terminating ApolloLink to request operations using given
  * AbsintheSocket instance
  */
-const createAbsintheSocketLink = <Result, Variables: void | Object>(
+const createAbsintheSocketLink = <Result, Variables extends void | Object>(
   absintheSocket: AbsintheSocket,
-  onError?: $ElementType<Observer<Result, Variables>, "onError">,
-  onStart?: $ElementType<Observer<Result, Variables>, "onStart">
+  onError?: $ElementType<Observer<Result, Variables>, 'onError'>,
+  onStart?: $ElementType<Observer<Result, Variables>, 'onStart'>,
 ) =>
   new ApolloLink(
     compose(
       notifierToObservable(absintheSocket, onError, onStart),
-      request => send(absintheSocket, request),
-      getRequest
-    )
+      (request) => send(absintheSocket, request),
+      getRequest,
+    ),
   );
 
 export default createAbsintheSocketLink;
